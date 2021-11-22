153bf8995c55be517e97bf198ff0006d87cb2e9b
MP3003K <mijailpalomino@upeu.edu.pe>
2021-11-22 17:08:22 -0500

    E2

--- application.js
+++ application.js
@@ -5,6 +5,8 @@ var routespsicologo = require('./routes/psicologos');
 var routespaciente = require('./routes/paciente');
 var routesatencion = require('./routes/atenciones');
 var routesImagen = require('./routes/imagen');
+var routesfiles = require('./routes/file');
+
 
 
 var application = express();
@@ -17,6 +19,8 @@ application.use('/api', routespsicologo);
 application.use('/api', routespaciente);
 application.use('/api', routesatencion);
 application.use('/datos',routesImagen );
+application.use(routesfiles);
+
 application.get('/health-check', function (req, resp) {
     resp.status(200).send({ mensaje: "OK" });
 })
