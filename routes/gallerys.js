12b1ca9d50d9ff1f322f60f96dcc5d651e94f220
MP3003K <mijailpalomino@upeu.edu.pe>
2021-11-24 23:16:39 -0500

    Base.v3.2

--- routes/gallerys.js
+++ routes/gallerys.js
@@ -27,6 +27,6 @@ application.delete('/gallery/delete', token.validarTokenDeUsuario , galleryscont
 
 application.get('/gallery/list', token.validarTokenDeUsuario , galleryscontroller.consultarFotosPorUsuario);
 
-application.get('/gallery/obtenerimagen/:imagename', token.validarTokenDeUsuario , galleryscontroller.retornarImagen);
+application.get('/gallery/obtenerimagen/:imagename' , galleryscontroller.retornarImagen);
 
 module.exports = application;
